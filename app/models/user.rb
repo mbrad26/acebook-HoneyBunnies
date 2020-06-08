0faf1380e12ed1faa93303b7223855beb11e03e2
Catriona <catrionamairi@blueyonder.co.uk>
2020-06-08 21:16:16 +0100

    Rubocop now passing and unique index added to email in users table

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
 
   has_secure_password
   validates :fname, :lname, :email, :password, presence: true
-  # validates_uniqueness_of :email
+  validates :email, uniqueness: true
   validates :password, length: { minimum: 5 }
-  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }, uniqueness: true
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200608123232) do
+ActiveRecord::Schema.define(version: 20200608201300) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 20200608123232) do
     t.string "profile_picture"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
