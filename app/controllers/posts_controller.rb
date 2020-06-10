1f92ac64a98384db56e2baa8df653eb523e17ab3
Ed <lookatemail@gmail.com>
2020-06-10 16:50:28 +0100

    added untracted files

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -35,7 +35,7 @@ class PostsController < ApplicationController
   private
 
   def post_params
-    params.require(:post).permit(:message)
+    params.require(:post).permit(:message, :image)
   end
 
   def new_post
--- spec/features/user_can_submit_posts_spec.rb
+++ spec/features/user_can_submit_posts_spec.rb
@@ -14,4 +14,22 @@ RSpec.feature 'Timeline', type: :feature do
 
     expect(page).to have_content('Hello, world!')
   end
+
+  scenario 'Can submit posts & image and view them' do
+    User.create(fname: 'Jon', lname: 'Doe', email: 'john.doe@example.com', password: 'password')
+    visit '/sessions/new'
+
+    fill_in 'user[email]', with: 'john.doe@example.com'
+    fill_in 'user[password]', with: 'password'
+    click_button 'Sign in'
+
+    fill_in 'Message', with: 'Hello, world!'
+    attach_file 'post[image]', "#{Rails.root}/spec/files/MVC_DataB.png"
+    find('[name=commit]').click
+    click_button 'Submit'
+
+    # expect(page).to have_content('Hello, world!')
+    # expect(page.find('.image')).to be
+    expect(page.has_css?('.image_class')).to be
+  end
 end
