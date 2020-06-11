f5692e395db1c7515d61f3c899de5d60474f116b
mbrad26 <mbrad26@gmail.com>
2020-06-11 09:12:09 +0100

    work in progress delete update picture

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ApplicationRecord
   belongs_to :user
   has_many :likes, dependent: :destroy
-  has_many_attached :images
+  has_many_attached :images, dependent: :destroy
 
   validates :message, presence: true
 end
--- spec/features/user_able_to_update_post_spec.rb
+++ spec/features/user_able_to_update_post_spec.rb
@@ -22,4 +22,29 @@ RSpec.feature 'Update Post', type: :feature do
     expect(page).to_not have_content('Hello, world!')
     expect(page).to have_content('New Message!')
   end
+
+
+  scenario 'user is able to delete a picture' do
+    user = User.create(fname: 'Jon', lname: 'Doe', email: 'john.doe@example.com', password: 'password')
+
+    visit '/sessions/new'
+
+    fill_in 'user[email]', with: 'john.doe@example.com'
+    fill_in 'user[password]', with: 'password'
+    click_button 'Sign in'
+
+    fill_in 'Message', with: 'Hello, world!'
+    attach_file 'post[images][]', "#{Rails.root}/spec/files/MVC_DataB.png"
+    click_button 'Submit'
+
+    all(:link, 'Update Post').last.click
+
+    click_link 'Delete'
+    # attach_file 'post[images][]', "#{Rails.root}/spec/files/Matterhorn.jpg"
+    click_button 'Submit'
+
+    expect(current_path).to eq '/posts'
+    expect(page).to have_content('Hello, world!')
+    expect(page).to_not have_css(".rounded")
+  end
 end
